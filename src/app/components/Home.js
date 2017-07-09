9429feeec2645527f4f7b33bf4b0aefd8fb410b3
Mustafa Sertac Turkel <sertac.turkel@etstur.com>
2017-07-09 20:19:59 +0300

    Two way binding

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,22 +1,18 @@
-/**
- * Created by sertac.turkel on 09/07/2017.
- */
 import React from "react";
-/**
- * Child Comp
- */
+
 export class Home extends React.Component {
     constructor(props) {
         super();
         this.state = {
             age: props.initialAge,
-            status: 0
+            status: 0,
+            homeLink: props.initialLinkName
         };
         setTimeout(() => {
             this.setState({
                 status: 1
-            })
-        }, 3000)
+            });
+        },3000);
     }
 
     onMakeOlder() {
@@ -25,16 +21,30 @@ export class Home extends React.Component {
         });
     }
 
+    onChangeLink() {
+        this.props.changeLink(this.state.homeLink);
+    }
+
+    onHandleChange(event) {
+        this.setState({
+            homeLink: event.target.value
+        });
+    }
+
     render() {
         return (
             <div>
-                <p>In a new Component!!!</p>
-                <p>Your name is {this.props.name} and your age is {this.state.age}</p>
-                <p>Status {this.state.status} </p>
+                <p>In a new Component!</p>
+                <p>Your name is {this.props.name}, your age is {this.state.age}</p>
+                <p>Status: {this.state.status}</p>
                 <hr/>
-                <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older!</button>
+                <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
                 <hr/>
                 <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
+                <hr/>
+                <input type="text" value={this.state.homeLink}
+                       onChange={(event) => this.onHandleChange(event)} />
+                <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Header Link</button>
             </div>
         );
     }
@@ -43,5 +53,6 @@ export class Home extends React.Component {
 Home.propTypes = {
     name: React.PropTypes.string,
     initialAge: React.PropTypes.number,
-    greet: React.PropTypes.func
-}
+    greet: React.PropTypes.func,
+    initialLinkName: React.PropTypes.string
+};
--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,25 @@
-/**
- * Created by sertac.turkel on 07/07/2017.
- */
-/**
- * FirstComponent
- * Parent
- */
 import React from "react";
 import {render} from "react-dom";
+
 import {Header} from "./components/Header";
 import {Home} from "./components/Home";
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            homeLink: "Home"
+        };
+    }
+
     onGreet() {
-        alert("Hello!")
+        alert("Hello!");
+    }
+
+    onChangeLinkName(newName) {
+        this.setState({
+            homeLink: newName
+        });
     }
 
     render() {
@@ -20,18 +27,23 @@ class App extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Header homeLink="Home"/>
+                        <Header homeLink={this.state.homeLink}/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"max"} initialAge={27} greet={this.onGreet}/>
+                        <Home
+                            name={"Max"}
+                            initialAge={27}
+                            greet={this.onGreet}
+                            changeLink={this.onChangeLinkName.bind(this)}
+                            initialLinkName={this.state.homeLink}
+                        />
                     </div>
                 </div>
             </div>
         );
     }
 }
+
 render(<App/>, window.document.getElementById("app"));
-
-
