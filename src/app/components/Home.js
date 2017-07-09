f739ca9d6c688031d7f3dfdb447341a218579d15
Mustafa Sertac Turkel <sertac.turkel@etstur.com>
2017-07-09 17:41:24 +0300

    Multiple Components

---
+++ src/app/components/Home.js
@@ -0,0 +1,14 @@
+/**
+ * Created by sertac.turkel on 09/07/2017.
+ */
+import React from "react";
+
+export class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>In a new Component!!!</p>
+            </div>
+        );
+    }
+}
