0c66663b36f5773487331184d71c88d9030c5bc4
Mustafa Sertac Turkel <sertac.turkel@etstur.com>
2017-07-09 17:25:50 +0300

    First Component

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,25 @@
 /**
  * Created by sertac.turkel on 07/07/2017.
  */
-console.log("It Works!");
+/**
+ * FirstComponent
+ */
+import React from "react";
+import {render} from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <h1>Hello!!!</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+render(<App/>, window.document.getElementById("app"));
+
+
