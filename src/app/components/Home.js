1afbd2428a56e056fc99363014b90f6aaed3d5e4
Mustafa Sertac Turkel <sertac.turkel@etstur.com>
2017-07-09 18:40:58 +0300

    Event view not changed because State is not changed.

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -4,23 +4,23 @@
 import React from "react";
 
 export class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.age = props.age;
+    }
+
+    onMakeOlder() {
+        this.age += 3;
+        console.log(this.age)
+    }
+
     render() {
-        console.log(this.props);
-        var text = "Something";
         return (
             <div>
                 <p>In a new Component!!!</p>
-                <p>Something is {text}</p>
-                <p>Your name is {this.props.name} and your age is {this.props.age}</p>
-                <p>USER object ==> Name: {this.props.user.name}</p>
-                <div>
-                    <h4>Hobbies</h4>
-                    <ul>
-                        {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-                    </ul>
-                </div>
+                <p>Your name is {this.props.name} and your age is {this.age}</p>
                 <hr/>
-                {this.props.children}
+                <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older!</button>
             </div>
         );
     }
@@ -28,7 +28,5 @@ export class Home extends React.Component {
 
 Home.propTypes = {
     name: React.PropTypes.string,
-    age: React.PropTypes.number,
-    user: React.PropTypes.object,
-    children: React.PropTypes.element.isRequired
+    age: React.PropTypes.number
 }
--- src/app/index.js
+++ src/app/index.js
@@ -11,12 +11,6 @@ import {Home} from "./components/Home";
 
 class App extends React.Component {
     render() {
-
-        var user={
-            name:"Anna",
-            hobbies:["Sports","Reading"]
-
-        };
         return (
             <div className="container">
                 <div className="row">
@@ -26,9 +20,7 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"max"} age={27} user={user}>
-                            <p>This is a Children!!!</p>
-                        </Home>
+                        <Home name={"max"} age={27}/>
                     </div>
                 </div>
             </div>
