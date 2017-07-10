88d776f62244331c570956f67d0c6292d3329c03
Mustafa Sertac Turkel <sertac.turkel@etstur.com>
2017-07-10 13:14:08 +0300

    Navigation and Url params

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,18 +2,19 @@
  * Created by sertac.turkel on 09/07/2017.
  */
 import React from "react";
+import {Link} from "react-router";
 
-export const Header =(props) => {
-
-        return (
-            <nav className="navbar navbar-default">
-                <div className="container">
-                    <div className="navbar-header">
-                        <ul className="nav navbar-nav">
-                            <li><a href="#">{props.homeLink}</a></li>
-                        </ul>
-                    </div>
+export const Header = (props) => {
+    return (
+        <nav className="navbar navbar-default">
+            <div className="container">
+                <div className="navbar-header">
+                    <ul className="nav navbar-nav">
+                        <li><Link to={"/home"} activeStyle={{color: "red"}}>Home</Link></li>
+                        <li><Link to={"/user/10"} activeClassName={"active"}>User</Link></li>
+                    </ul>
                 </div>
-            </nav>
-        );
-}
+            </div>
+        </nav>
+    );
+};
---
+++ src/app/components/Root.js
@@ -0,0 +1,26 @@
+/**
+ * Created by sertac.turkel on 10/07/2017.
+ */
+import React from "react";
+
+import {Header} from "./Header";
+
+export class Root extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Header />
+                    </div>
+                </div>
+                <hr/>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        {this.props.children}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
