6b8f3f29b495e889d5db3ff77785d78217b376c3
Mustafa Sertac Turkel <sertac.turkel@etstur.com>
2017-07-10 17:19:24 +0300

    Multiple reducer

--- src/app/index.js
+++ src/app/index.js
@@ -1,32 +1,53 @@
-import { createStore } from "redux";
-const initialState = {
+import {createStore, combineReducers } from "redux";
+
+const mathReducer = (state = {
     results: 1,
-    lastValues: [],
-    username: "Max"
-};
-const reducer = (state = initialState,action)=>{
-    switch (action.type){
+    lastValues: []
+}, action) => {
+    switch (action.type) {
         case "ADD":
             state = {
                 ...state,
-                result:state.results + action.payload,
-                lastValues: [...state.lastValues,action.payload]
+                result: state.results + action.payload,
+                lastValues: [...state.lastValues, action.payload]
             };
 
             break;
         case "SUBTRACT":
             state = {
                 ...state,
-                result:state.results - action.payload,
-                lastValues: [...state.lastValues,action.payload]
+                result: state.results - action.payload,
+                lastValues: [...state.lastValues, action.payload]
             };
             break;
     }
     return state;
 };
-const store = createStore(reducer);
-store.subscribe(()=>{
-    console.log("Store updated!",store.getState());
+
+const userReducer = (state = {
+    name: "Max",
+    age:27
+}, action) => {
+    switch (action.type) {
+        case "SET_NAME":
+            state = {
+                ...state,
+                name: action.payload
+            };
+
+            break;
+        case "SET_AGE":
+            state = {
+                ...state,
+                age: action.payload
+            };
+            break;
+    }
+    return state;
+};
+const store = createStore(combineReducers({mathReducer,userReducer}));
+store.subscribe(() => {
+    console.log("Store updated!", store.getState());
 })
 store.dispatch({
     type: "ADD",
@@ -35,4 +56,8 @@ store.dispatch({
 store.dispatch({
     type: "SUBTRACT",
     payload: 82
+});
+store.dispatch({
+    type: "SET_AGE",
+    payload: 30
 });
