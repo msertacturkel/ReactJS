87ec4117618d89195ef334cdcb5edfca8bf1d825
Mustafa Sertac Turkel <sertac.turkel@etstur.com>
2017-07-10 15:43:56 +0300

    Redux intro

--- src/app/index.js
+++ src/app/index.js
@@ -1,24 +1,24 @@
-import React from "react";
-import {render} from "react-dom";
-import {Router, Route, browserHistory, IndexRoute} from "react-router";
-
-import {Root} from "./components/Root";
-import {Home} from "./components/Home";
-import {User} from "./components/User";
-
-class App extends React.Component {
-    render() {
-        return (
-            <Router history={browserHistory}>
-                <Route path={"/"} component={Root} >
-                    <IndexRoute component={Home} />
-                    <Route path={"user/:id"} component={User} />
-                    <Route path={"home"} component={Home} />
-                </Route>
-                <Route path={"home-single"} component={Home}/>
-            </Router>
-        );
+import { createStore } from "redux";
+const reducer = (state,action)=>{
+    switch (action.type){
+        case "ADD":
+            state = state + action.payload;
+            break;
+        case "SUBTRACT":
+            state = state - action.payload;
+            break;
     }
-}
-
-render(<App />, window.document.getElementById('app'));
+    return state;
+};
+const store = createStore(reducer,1);
+store.subscribe(()=>{
+    console.log("Store updated!",store.getState());
+})
+store.dispatch({
+    type: "ADD",
+    payload: 10
+});
+store.dispatch({
+    type: "SUBTRACT",
+    payload: 82
+});
